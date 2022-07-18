8b257afff4e6fb4cde7f81f55d267d855abf3f6e
"Aloysiu" <"aloycwl@gmail.com">
2022-07-18 16:38:39 +0800

    mint and burn

--- ERC20AC_RG.sol
+++ ERC20AC_RG.sol
@@ -2,16 +2,20 @@ pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
 import"https://github.com/aloycwl/ERC_AC/blob/main/Util/OnlyAccess.sol";
 contract RG is ERC20AC,OnlyAccess{
-    constructor(address a,string memory name_,string memory sym_)ERC20AC(name_,sym_){
-        (_balances[msg.sender],_balances[a],_con,_totalSupply)=(95e25,5e25,a,1e27);
-    }
+    constructor(string memory name_,string memory sym_)ERC20AC(name_,sym_){}
     function transferFrom(address a,address b,uint c)public override returns(bool){unchecked{
         require(_balances[a]>=c);
         require(a==msg.sender||_allowances[a][b]>=c);
-        if(msg.sender==_con)_fromCon[b]=_con;
         (_balances[a]-=c,_balances[b]+=c);
         emit Transfer(a,b,c);
         return true;
     }}
-
+    function mint(address a,uint b)external onlyAccess{unchecked{
+        (_balances[a]+=b,_totalSupply+=b);
+        emit Transfer(address(this),a,b);
+    }}
+    function burn(uint a)external onlyAccess{unchecked{
+        _totalSupply-=a;
+        emit Transfer(address(this),address(0),a);
+    }}
 }
--- ERC721AC_RG.sol
+++ ERC721AC_RG.sol
@@ -5,6 +5,7 @@ interface IERC20{
     function transferFrom(address,address,uint)external;
     function balanceOf(address)external view returns(uint);
     function allowance(address,address)external view returns(uint);
+    function mint()
 }
 contract RG is ERC721AC,OnlyAccess{
     uint public _count=1;
