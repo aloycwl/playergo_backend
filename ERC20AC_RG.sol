4afaeff0f26eeda10785be7b382c215a47548a75
"Aloysiu" <"aloycwl@gmail.com">
2022-07-18 15:27:56 +0800

    GetTokens

--- ERC20AC_RG.sol
+++ ERC20AC_RG.sol
@@ -2,22 +2,16 @@ pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
 import"https://github.com/aloycwl/ERC_AC/blob/main/Util/OnlyAccess.sol";
 contract RG is ERC20AC,OnlyAccess{
-    mapping(address=>address)private _fromCon;
-    address private _con;
-    uint public _released;
     constructor(address a,string memory name_,string memory sym_)ERC20AC(name_,sym_){
         (_balances[msg.sender],_balances[a],_con,_totalSupply)=(95e25,5e25,a,1e27);
     }
     function transferFrom(address a,address b,uint c)public override returns(bool){unchecked{
         require(_balances[a]>=c);
         require(a==msg.sender||_allowances[a][b]>=c);
-        require(_released>0||_fromCon[a]!=_con);
         if(msg.sender==_con)_fromCon[b]=_con;
         (_balances[a]-=c,_balances[b]+=c);
         emit Transfer(a,b,c);
         return true;
     }}
-    function toggleRelease()external onlyAccess{
-        _released=_released==0?1:0;
-    }
+
 }
--- ERC721AC_RG.sol
+++ ERC721AC_RG.sol
@@ -1,23 +1,24 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC721AC/ERC721AC.sol";
+import"https://github.com/aloycwl/ERC_AC/blob/main/Util/OnlyAccess.sol";
 interface IERC20{
     function transferFrom(address,address,uint)external;
     function balanceOf(address)external view returns(uint);
     function allowance(address,address)external view returns(uint);
 }
-contract RG is ERC721AC{
+contract RG is ERC721AC,OnlyAccess{
     uint public _count=1;
+    uint private _release;
+    mapping(address=>uint[])private _tokens;
     IERC20 private ipg;
     IERC20 private iusdt;
     constructor(string memory name_,string memory sym_,address a)ERC721AC(name_,sym_){
         iusdt=IERC20(a);
     }
-
     function tokenURI(uint)external pure override returns(string memory){
         return"https://ipfs.io/ipfs/bafybeieuti6mhg5p6pbf7n4emjqff5l6b4qv5pm7fbhqfy2i3rialwp52y/rg.json";
     }
-
-    function Mint(address a)external{unchecked{
+    function mint(address a)external{unchecked{
         require(_count<5e3,"Token sales is over");
         require(iusdt.balanceOf(msg.sender)>=1e21,"Insufficient USDT");
         require(iusdt.allowance(msg.sender,address(this))>=1e21,"Insufficient allowance");
@@ -28,5 +29,20 @@ contract RG is ERC721AC{
         emit Transfer(address(0),msg.sender,_count);
         _count++;
     }}
-
+    function burn(uint a)external onlyAccess{unchecked{
+        require(msg.sender==_owner);
+        _count-=a;
+    }}
+    function toggleRelease()external onlyAccess{
+        _release=_release==0?block.timestamp:0;
+    }
+    function getDrip()public view returns(uint){
+        return _release>0?block.timestamp-_release:0;
+    }
+    function drip()external{
+        
+    }
+    function getTokens()external view returns(uint[]memory){
+        return _tokens[msg.sender];
+    }
 }
